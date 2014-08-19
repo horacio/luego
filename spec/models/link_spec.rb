67f20be39d26894f0184ba29a1f6d975d1836c05
Horacio Bertorello <syrii@msn.com>
2014-08-19 12:02:07 -0300

    Move validation to a more meaningful code block.

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 describe Link, 'validations' do
-  it { should belong_to(:user) }
   it { should validate_presence_of :url }
   it { should validate_uniqueness_of :url }
+end
+
+describe Link, 'associations' do
+  it { should belong_to(:user) }
 end
